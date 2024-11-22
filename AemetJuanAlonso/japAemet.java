5ea5e039e27937c48d446636e878efaa1b9fe354
juanalonso21 <juanalonsopulpi@gmail.com>
2024-11-22 13:16:22 +0100

    Ultimo Commit

--- AemetJuanAlonso/japAemet.java
+++ AemetJuanAlonso/japAemet.java
@@ -6,22 +6,42 @@ public class japAemet {
     static String[]  japNombres = {"Almeria", "Sevilla", "Malaga", "Cordoba", "Jaen", "Granada", "Huelva", "Cadiz"};
     static int vmax[] = new int[japNombres.length];
     static int vmin[] = new int [japNombres.length];
-        public static void main(String[] args) {
-            japRegCiudad V = new japRegCiudad();
-            japgenerarTemperaturas();
-        }
-        public static void japgenerarTemperaturas(){        
-            for (int i = 0; i < japNombres.length; i++) {
-                    System.out.println(japNombres[i]);
-                    System.out.println("    -Vmax:");
-                    vmax[i] = sc.nextInt();
-                    System.out.println("    -Vmin:");
-                    vmin[i] = sc.nextInt();
-                    japRegCiudad V = new japRegCiudad(vmax[i], vmin[i], japNombres[i]);
+    static int[][] registrotemperatura;
+    static japRegCiudad[] japRegCiudad= new japRegCiudad[8];
+        
+            public static void main(String[] args) {
+    
+                japgenerarTemperatura();
+                // asignacion();
+                japImprimirDatos();
+            }
+            public static void japgenerarTemperatura(){        
+                for (int i = 0; i < japNombres.length; i++) {
+                        System.out.println(japNombres[i]);
+                        System.out.println("    -Vmax:");
+                        vmax[i] = sc.nextInt();
+                        System.out.println("    -Vmin:");
+                        vmin[i] = sc.nextInt();
+                        japRegCiudad[i]= new japRegCiudad(vmax[i], vmin[i], japNombres[i], null);
         }
     }
-    public static void ListarRegistroTemp(String[] args) {
-        V.visualizacionClase;
+    public static int getTempMediaDia(String dia){
+        int sumatotal =0;
+        for( int i= 0; i> 7; i++){
+          sumatotal = japRegCiudad[i].sumaPorDia()[i] + sumatotal;  
+        }
+        sumatotal = sumatotal/8;
+        return sumatotal;
+    }
+    public static void japImprimirDatos() {
+        for (int i = 0; i < japNombres.length; i++) {
+            japRegCiudad[i].visualizacionClase();
+        }
+        japRegCiudad[0].visualizacionDatosDia("Lunes");
+        System.out.println("La temperatura media el Lunes es de: " + getTempMediaDia("Lunes"));
     }
 
-}
+
+
+    }
+
--- AemetJuanAlonso/japRegCiudad.java
+++ AemetJuanAlonso/japRegCiudad.java
@@ -3,37 +3,103 @@ import java.util.Scanner;
 public class japRegCiudad {
     int NDIAS = 7;
     int NTEMP = 3;
+    String[] dias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
     private String nombre;
-    int tmax;
-    int tmin;
-    String ciudad;
-    int[][] temperaturas = new int[NDIAS][NTEMP];
+    private int tmax;
+    private int tmin;
+    private String ciudad;
+    public int[][] temperaturas = new int[NDIAS][NTEMP];
     static Scanner sc = new Scanner(System.in);
 
-    public japRegCiudad(int vmax, int vmin, String ciudad) {
-        int tmax = vmax;
-        int tmin = vmin;
-        ciudad = nombre;
+    public japRegCiudad(int vmax, int vmin, String ciudad, int[][] temperatura) {
+        this.tmax = vmax;
+        this.tmin = vmin;
+        this.ciudad = ciudad;
+        temperatura = this.japgenerarTemperaturas(this.tmax, this.tmin);
     }
-    public static void main(String[] args) {
 
-    }
-    public static void getIndexCiudad() {
-        
-    }
-    public void japgenerarTemperaturas(){
+    public int[][] japgenerarTemperaturas(int tmax,int tmin){
+        tmax = this.tmax;
+        tmin = this.tmin;
         for (int i = 0; i < NDIAS; i++) {
             for (int j = 0; j < NTEMP; j++) {
-                temperaturas[i][j] = (int) (Math.random() * tmax - tmin);
+                temperaturas[i][j] = (int) (Math.random() * tmax + tmin);
             }
         }
+        return temperaturas;
     }
     public void visualizacionClase() {
         for (int i = 0; i < NDIAS; i++) {
+            System.out.println("Temperaturas en "+ ciudad +" "+ "el " + dias[i]);
             for (int j = 0; j < NTEMP; j++) {
-                System.out.println("Temperaturas en "+ ciudad);
+
                 System.out.println(temperaturas[i][j]);
             }
         }
+    }
+    private int getIndexDia(String dia){
+        int index = -1;
+        for (int i = 0; i < dias.length; i++) {
+            if (dias[i].equals(dia)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    public int[] getDatosDia(String dia){
+        int dias = getIndexDia(dia);
+        int suma =0;
+        int media;
+        int max = -100;
+        int min = 10000;
+        int[] estadisticas = new int[3];
+        for (int i = 0; i < NTEMP; i++) {
+            if (i ==0){
+                max = temperaturas[dias][i];
+                min = temperaturas[dias][i];
+            }
+            suma = suma + temperaturas[dias][i];
+            if (temperaturas[dias][i] > max){
+                max = temperaturas[dias][i];
+            }
+            if (temperaturas[dias][i] < min){
+                min = temperaturas[dias][i];
+            }
+            
+        } 
+        media = suma / NTEMP;  
+        estadisticas[0] = media;
+        estadisticas[1] = max;
+        estadisticas[2] = min;
+        return estadisticas; 
+       
+    }
+    public int[] maximapordia() {
+        int[] maximapordia = new int[8];
+        for(int i=0; i>6; i++){
+            for(int j=0; j>2; j++){
+                if (temperaturas[i][j] > maximapordia[i]){
+                    maximapordia[i] = temperaturas[i][j];
+                }
+            }
+        }
+        return maximapordia;
+    }
+    public int[] sumaPorDia() {
+        int[] sumaporDia = new int[8];
+        for(int i=0; i>6; i++){
+            for(int j=0; j>2; j++){
+
+                sumaporDia[j] = sumaporDia[j] + temperaturas[i][j];
+            }
+        }
+        return sumaporDia;
+    }
+    
+    public void visualizacionDatosDia(String dia){
+        int[] estadisticas = getDatosDia(dia);
+        System.out.println("La temperatura media es: " + estadisticas[0]);
+        System.out.println("La temperatura maxima es: " + estadisticas[1]);
+        System.out.println("La temperatura minima es: " + estadisticas[2]);
     }
 }
